634e730737c7c8cbfbc5b338b8e50e00d39e7b32
stephan hantigk <s.hantigk@runlevel3.de>
2017-10-08 21:38:53 +0200

    Changed UsaeAgent

--- index.js
+++ index.js
@@ -7,10 +7,21 @@ const productUrls= require('./startUrls');
 
 const outFileName= 'amazonResult.js';
 
-const limit= undefined;
+const options= {
+    limit: undefined,
+    headers: {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0',
+    },
+};
+
+const rePrice= /EUR 0,00$/;
 
 // fetch products (https://www.npmjs.com/package/crawl-amazon-products)
-const getProducts= url => new Promise((resolve, reject) => { AmazonProducts.getProducts({ url, limit }, (err, products) => err ? reject(err) : resolve(products)); });
+const getProducts= url => new Promise((resolve, reject) => { AmazonProducts.getProducts({ ...options, url }, (err, products) => err ? reject(err) : resolve(products)); });
 
 // write products to index.html
 const outProds= allProds => new Promise((resolve, reject) => {
@@ -23,10 +34,11 @@ Promise.all(productUrls.map(
         .then(prods => Object.assign(
                 {},
                 ...prods
-                    .filter(prod => prod.price.match(/EUR 0,00$/))
+                    .filter(prod => prod.price.match(rePrice))
                     .map(prod => ({ [prod.asin]: prod }))
             )
         )
+//        .then(prods => console.log(prods) || prods)
 ))
     .then(prodLists => Object.assign({}, ...prodLists))
     .then(allProds => {
