71a5c19e486696172c33b0ec1169fceabf0e0e9f
stephan hantigk <s.hantigk@runlevel3.de>
2017-10-09 22:35:12 +0200

    Parse pages in sequence change user agent for each query

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const productUrls= require('./startUrls');
 
 const outFileName= 'amazonResult.js';
 
-const options= {
+const options= () => ({
     limit: undefined,
     headers: {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.8',
         'Cache-Control': 'no-cache',
         'Connection': 'keep-alive',
-        'User-Agent': 'Mozilla/5.0 (X11a; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:55.0.' + Math.random() + ') Gecko/20100101 Firefox/55.0',
     },
-};
+});
 
-const mergeObjects= function( objs ) {
+const mergeObjects= objs => {
     let result= {};
     objs.forEach(obj => Object.assign(result, obj));
     return result;
@@ -27,7 +27,7 @@ const mergeObjects= function( objs ) {
 const rePrice= /EUR 0,00$/;
 
 // fetch products (https://www.npmjs.com/package/crawl-amazon-products)
-const getProducts= url => new Promise((resolve, reject) => { console.log('Fetching ' + url + '...'); AmazonProducts.getProducts(mergeObjects([ options, { url: url } ]), (err, products) => err ? reject(err) : resolve(products)); });
+const getProducts= url => new Promise((resolve, reject) => { console.log('Fetching ' + url + '...'); AmazonProducts.getProducts(mergeObjects([ options(), { url: url } ]), (err, products) => err ? reject(err) : resolve(products)); });
 
 // write products to index.html
 const outProds= allProds => new Promise((resolve, reject) => {
@@ -37,8 +37,19 @@ const outProds= allProds => new Promise((resolve, reject) => {
 
 const promiseLog= message => res => { console.log(message); return res; };
 
-Promise.all(productUrls.map(
-    url => getProducts(url)
+const runAllParallel= pFns => Promise.all(pFns.map(fn => fn()));
+const runAllSeriell= pFns => {
+    let p= Promise.resolve([]);
+    pFns.forEach(fn => {
+        p= p.then(result => fn().then(r => result.push(r)).then(() => result));
+    })
+    return p;
+};
+
+const runAll= runAllSeriell;
+
+runAll(productUrls.map(
+    url => () => getProducts(url)
         .then(prods => mergeObjects(
                 prods
                     .filter(prod => prod.price.match(rePrice))
